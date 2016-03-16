563f866e1c1d624014b97d4292ba82ffe910c7f7
Samuel Spencer <theodore.therone@gmail.com>
2016-03-16 08:57:59 +0000

    outcome screens improved, new migration

--- comet/urls.py
+++ comet/urls.py
@@ -2,12 +2,28 @@ from django.conf.urls import include, patterns, url
 
 from comet import views#,forms
 from django.views.generic import TemplateView
-
+from aristotle_mdr.contrib.generic.views import GenericAlterManyToManyView
+from comet import models
 urlpatterns = patterns('comet.views',
     url(r'^/?$', TemplateView.as_view(template_name='comet/static/about_comet_mdr.html')),
     url(r'^indicator/(?P<iid>\d+)?/?$', views.indicator, name='indicator'),
     url(r'^qualitystatement/(?P<iid>\d+)/?$', views.qualitystatement, name='qualitystatement'),
     url(r'^datasource/(?P<iid>\d+)/?$', views.datasource, name='datasource'),
+    url(r'^outcome_area/(?P<iid>\d+)/add_indicators/?$', views.OutcomeAlterIndicators.as_view(), name='outcomearea_alter_indicators'),
+    url(r'^outcome_area/(?P<iid>\d+)/add_indicators/?$',
+        GenericAlterManyToManyView.as_view(
+            model_base = models.OutcomeArea,
+            model_to_add = models.Indicator,
+            model_base_field = 'indicators',
+        ), name='outcomearea_alter_indicators'),
+    url(r'^indicator/(?P<iid>\d+)/add_outcome_areas/?$',
+        GenericAlterManyToManyView.as_view(
+            model_base = models.Indicator,
+            model_to_add = models.OutcomeArea,
+            model_base_field = 'outcome_areas',
+        ),
+        name='indicator_alter_outcomeareas'
+    ),
 
 #These are required for about pages to work. Include them, or custom items will die!
     url(r'^about/(?P<template>.+)/?$', views.DynamicTemplateView.as_view(), name="about"),
--- comet/views.py
+++ comet/views.py
@@ -1,4 +1,5 @@
 import aristotle_mdr as aristotle
+from aristotle_mdr.contrib.generic.views import GenericAlterManyToManyView
 import comet
 from django.views.generic import TemplateView
 
@@ -14,3 +15,8 @@ def qualitystatement(*args,**kwargs):
 class DynamicTemplateView(TemplateView):
     def get_template_names(self):
         return ['comet/static/%s.html' % self.kwargs['template']]
+
+class OutcomeAlterIndicators(GenericAlterManyToManyView):
+    model_base = comet.models.OutcomeArea
+    model_to_add = comet.models.Indicator
+    model_base_field = 'indicators'
