7d08ca73fcea995d4e6655dd9eea23d850aeb3a3
Samuel Spencer <theodore.therone@gmail.com>
2017-10-30 05:49:29 +0000

    remove null field, add manifest

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 import os
-from setuptools import setup
+from setuptools import setup, find_packages
 
 with open(os.path.join(os.path.dirname(__file__), 'README.rst')) as readme:
     README = readme.read()
@@ -10,7 +10,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 setup(
     name='django-aristotle-comet-indicator-registry',
     version='1.6',
-    packages=['comet'],
+    packages=find_packages(),
     include_package_data=True,
     license='BSD Licence',
     description='',
